d6f1bff62f84c68a24e4dff76f775b9c8a9c35f5
Jodu555 <Jodu505@gmail.com>
2021-09-08 10:00:43 +0200

    Implemented socket.io for later usage

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
-const server = express();
+const http = require('http');
+const app = express();
+const server = http.Server(app);
+const io = require('socket.io')(server);
 const { Database } = require('@jodu555/mysqlapi');
 const database = Database.createDatabase('localhost', 'root', '', 'yt-chatbot');
 database.connect();
 require('./tables').create();
 
 const YouTubeApi = require('./YouTubeApi');
-
-
 const youtubeApi = new YouTubeApi();
 
 youtubeApi.setCallback('init', async () => {
@@ -16,7 +17,6 @@ youtubeApi.setCallback('init', async () => {
   youtubeApi.getLiveChatInteractions().startMessageTracking();
   // youtubeApi.getLiveChatInteractions().insertChatMessage('Hello from the NodeJS Application Second');
 });
-
 youtubeApi.registerCommand('!uptime', (command, message) => {
   return 'Der Stream ist bereits seit ' + youtubeApi.getLiveChatInteractions().getStreamUptime() + ' Live!'
 });
@@ -36,15 +36,14 @@ youtubeApi.registerCommand('!coins', async (command, message) => {
 
 youtubeApi.setCallback('newMessage', (message) => {
   // console.log(message);
-
 });
 
-server.get('/authorize', (request, response) => {
+app.get('/authorize', (request, response) => {
   console.log('/auth');
   response.redirect(youtubeApi.getOAuth().getAuthURL());
 });
 
-server.get('/callback', (req, response) => {
+app.get('/callback', (req, response) => {
   console.log('/callback');
   const { code } = req.query;
   youtubeApi.getOAuth().authorize(code);
